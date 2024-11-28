ef80db61fd4b47991acffee3a1c183d73151a784
ljy0711 <wjddus5696@naver.com>
2024-11-28 10:19:49 +0900

    Add files via upload

---
+++ 018_inheritance/018_inheritance.cpp
@@ -0,0 +1,42 @@
+﻿// 018_inheritance.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+// 018_inheritance.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    int bNum;
+    void bFunc() {
+        cout << "hello from Base!" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    int dNum;
+    void dFunc() {
+        cout << "Hello from Derived!" << endl;
+    }
+};
+int main()
+{
+    Base b;
+    Derived d;
+
+    b.bFunc();
+    b.bNum = 1;
+    cout << "b.bNum = " << b.bNum << endl;
+
+    d.bFunc();
+    d.bNum = 2;
+    cout << "d.bNum = " << d.bNum << endl;
+
+    d.dFunc();
+    d.dNum = 1;
+    cout << "d.dNum = " << d.dNum << endl;
+}
+
+
---
+++ 019_animal/019_animal.cpp
@@ -0,0 +1,39 @@
+﻿// 019_animal.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    int age;
+    Animal() { cout << "Animal 생성자 실행!" << endl; }
+    ~Animal() { cout << "Animal 소멸자 실행!" << endl; }
+    void Breathe() { cout << "숨을 쉰다" << endl; }
+};
+class Dog : public Animal {
+public:
+    Dog() { cout << "Dog 생성자 실행!" << endl; }
+    ~Dog() { cout << "Dog 소멸자 실행!" << endl; }
+    void Walk() { cout << "걷는다" << endl; }
+};
+class Sparrow : public Animal {
+public:
+    Sparrow() { cout << "Sparrow  생성자 실행!" << endl; }
+    ~Sparrow() { cout << "Sparrow  소멸자 실행!" << endl; }
+    void Fly() { cout << "난다" << endl; }
+};
+int main()
+{
+    Dog d;
+    d.age = 7;
+    d.Breathe();
+    d.Walk();
+    cout << d.age << "살" << endl;
+
+
+    Sparrow s;
+    s.age = 2;
+    s.Breathe();
+    s.Fly();
+    cout << s.age << "살" << endl;
+}
